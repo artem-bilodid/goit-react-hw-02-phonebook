159c1d9de31568ea9629c2e41ac594fde316d719
Artem Bilodid <ar.bilodid@draftkings.com>
2021-11-01 14:07:49 +0200

    finished delete logic. removed mock data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,16 +8,18 @@ import Filter from '../filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
-  handleChange = event => {
+  handleContactDelete = event => {
+    const { id } = event.target;
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleFilterChange = event => {
     const { name, value } = event.target;
 
     this.setState({ [name]: value });
@@ -59,8 +61,8 @@ class App extends Component {
         <ContactForm onSubmit={this.addContact} />
 
         <h2>Contacts</h2>
-        <Filter filter={filter} handleChange={this.handleChange} />
-        <ContactList contacts={filteredContacts} />
+        <Filter filter={filter} handleChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} handleDelete={this.handleContactDelete} />
       </div>
     );
   }
--- src/components/contact-list/contact-list.js
+++ src/components/contact-list/contact-list.js
@@ -1,13 +1,20 @@
-// import s from './contact-list.module.scss';
+import s from './contact-list.module.scss';
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, handleDelete }) => {
   const contactItems = contacts.map(({ id, name, number }) => (
-    <li key={id}>
-      {name}: {number}
+    <li key={id} className={s.item}>
+      <div className={s.textContainer}>
+        <span>
+          {name}: {number}
+        </span>
+        <button id={id} className={s.button} type="button" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
     </li>
   ));
 
-  return <ul>{contactItems}</ul>;
+  return <ul className={s.list}>{contactItems}</ul>;
 };
 
 export default ContactList;
